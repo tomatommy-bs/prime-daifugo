e5efb6a58d6702b2fa9f74f4ecf22e57c8b47470
oura-hideyoshi <tomatommy.bs@gmail.com>
2024-07-06 02:20:15 +0900

    mantine css

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, ColorSchemeScript } from "@mantine/core";
+import "@mantine/core/styles.css";
 
 const geistSans = localFont({
   src: "../fonts/GeistVF.woff",
@@ -24,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <ColorSchemeScript />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <MantineProvider>{children}</MantineProvider>
       </body>
