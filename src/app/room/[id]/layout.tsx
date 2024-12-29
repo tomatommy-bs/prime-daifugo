78aa52d76a4250128ba16dc44f2361caad3a9fd3
大浦秀喜 <h.oura@strategit.jp>
2024-12-29 10:59:47 +0900

    ✨ feat: add @tabler/icons-react dependency and update layout metadata; integrate new icons for enhanced UI in game room and update page titles and descriptions for improved SEO and user engagement.

--- src/app/room/[id]/layout.tsx
+++ src/app/room/[id]/layout.tsx
@@ -1,16 +1,29 @@
-import { Affix, Button, Container } from '@mantine/core'
+'use client'
+
+import { useShouldDevice } from '@/hooks/use-should-device'
+import { ActionIcon, Affix, Button, Container, Group } from '@mantine/core'
+import { IconMaximize, IconMinimize } from '@tabler/icons-react'
 import Link from 'next/link'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const { canFullscreen, shouldFullscreen, toggleFullscreen } = useShouldDevice({})
+
   return (
     <section>
       <Container>
         <main>{children}</main>
         <footer>
           <Affix className="m-4" position={{ right: 0, top: 0 }}>
-            <Link href="/">
-              <Button>Home</Button>
-            </Link>
+            <Group>
+              {canFullscreen && (
+                <ActionIcon onClick={toggleFullscreen}>
+                  {shouldFullscreen ? <IconMaximize /> : <IconMinimize />}
+                </ActionIcon>
+              )}
+              <Link href="/">
+                <Button>Home</Button>
+              </Link>
+            </Group>
           </Affix>
         </footer>
       </Container>
