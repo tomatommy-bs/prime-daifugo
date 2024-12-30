8c48f903f401d8cecc0d6e45383889cb11f75025
大浦秀喜 <h.oura@strategit.jp>
2024-12-30 11:51:44 +0900

    ✨ feat: enhance layout with burger menu and fullscreen toggle; replace action icons with a burger menu for improved navigation, allowing users to access the home link and fullscreen option more intuitively in the game room UI.

--- src/app/room/[id]/layout.tsx
+++ src/app/room/[id]/layout.tsx
@@ -1,12 +1,14 @@
 'use client'
 
 import { useShouldDevice } from '@/hooks/use-should-device'
-import { ActionIcon, Affix, Button, Container, Group } from '@mantine/core'
-import { IconMaximize, IconMinimize } from '@tabler/icons-react'
+import { Affix, Burger, Container, Menu } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
+import { IconHome, IconMaximize, IconMinimize } from '@tabler/icons-react'
 import Link from 'next/link'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const { canFullscreen, shouldFullscreen, toggleFullscreen } = useShouldDevice({})
+  const [burgerOpened, burgerHandler] = useDisclosure()
 
   return (
     <section>
@@ -14,16 +16,24 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <main>{children}</main>
         <footer>
           <Affix className="m-4" position={{ right: 0, top: 0 }}>
-            <Group>
-              {canFullscreen && (
-                <ActionIcon onClick={toggleFullscreen}>
-                  {shouldFullscreen ? <IconMaximize /> : <IconMinimize />}
-                </ActionIcon>
-              )}
-              <Link href="/">
-                <Button>Home</Button>
-              </Link>
-            </Group>
+            <Menu opened={burgerOpened} onClose={burgerHandler.close}>
+              <Menu.Target>
+                <Burger opened={burgerOpened} onClick={burgerHandler.toggle} />
+              </Menu.Target>
+              <Menu.Dropdown>
+                <Link href="/">
+                  <Menu.Item leftSection={<IconHome />}>Home</Menu.Item>
+                </Link>
+                {canFullscreen && (
+                  <Menu.Item
+                    leftSection={shouldFullscreen ? <IconMaximize /> : <IconMinimize />}
+                    onClick={toggleFullscreen}
+                  >
+                    Fullscreen
+                  </Menu.Item>
+                )}
+              </Menu.Dropdown>
+            </Menu>
           </Affix>
         </footer>
       </Container>
