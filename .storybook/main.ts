cc584e5d4f7c144e80dd1f378d4cba7d59e8b4ef
tomatommy-bs <tomatommy.bs@gmail.com>
2025-06-18 10:23:59 +0900

    fix: update Storybook configuration and dependencies to specific versions

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,18 +5,29 @@ const config: StorybookConfig = {
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
+
   "addons": [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
     "@storybook/experimental-addon-test"
   ],
+
   "framework": {
     "name": "@storybook/experimental-nextjs-vite",
     "options": {}
   },
+
   "staticDirs": [
     "../public"
-  ]
+  ],
+
+  docs: {
+    autodocs: true
+  },
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 export default config;
